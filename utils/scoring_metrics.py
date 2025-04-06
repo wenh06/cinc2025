38400d03650aae558be1193bb5baca71ad5e2398
WEN Hao <wenh06@gmail.com>
2025-04-06 11:02:29 +0800

    enhance scoring metrics computation

--- utils/scoring_metrics.py
+++ utils/scoring_metrics.py
@@ -5,7 +5,7 @@ import torch
 from torch_ecg.components.metrics import ClassificationMetrics
 from torch_ecg.utils.misc import make_serializable
 
-from helper_code import compute_accuracy, compute_auc, compute_challenge_score, compute_f_measure
+from helper_code import compute_accuracy, compute_auc, compute_confusion_matrix, compute_f_measure
 from outputs import CINC2025Outputs
 
 __all__ = [
@@ -127,6 +127,7 @@ def compute_chagas_metrics(
     auroc, auprc = compute_auc(labels, probability_outputs)
     accuracy = compute_accuracy(labels, binary_outputs)
     f_measure = compute_f_measure(labels, binary_outputs)
+    tpr = compute_chagas_tpr(labels, binary_outputs)
 
     return make_serializable(
         {
@@ -135,8 +136,122 @@ def compute_chagas_metrics(
             "auprc": auprc,
             "accuracy": accuracy,
             "f_measure": f_measure,
+            "tpr": tpr,
         }
     )
+
+
+def compute_chagas_tpr(
+    labels: np.ndarray,
+    binary_outputs: np.ndarray,
+) -> float:
+    """Compute the TPR for the "chagas" prediction (binary classification) task.
+
+    Parameters
+    ----------
+    labels : np.ndarray
+        The labels for the records, of shape `(num_samples,)` with binary values.
+    binary_outputs : np.ndarray
+        The binary outputs for the records, of shape `(num_samples,)` with binary values.
+
+    Returns
+    -------
+    float
+        The computed TPR for the "chagas" prediction task.
+
+    """
+    A = compute_confusion_matrix(labels, binary_outputs)
+    tp, fp, fn, tn = A[0, 0], A[0, 1], A[1, 0], A[1, 1]
+    tpr = tp / (tp + fn) if (tp + fn) > 0 else np.nan
+
+    return tpr
+
+
+def compute_challenge_score(labels, probability_outputs, max_fraction_positive=0.05):
+    """Compute the challenge score for the "chagas" prediction (binary classification) task.
+
+    The challenge score is defined as the true positive rate (TPR) for the model outputs,
+    with the constraint that the number of positive model outputs is no more than 5% of the total instances.
+    The TPR is defined as the number of true positive instances divided by the total number of positive instances.
+
+    This function is copied from helper_code.py,
+    and some printings are added to help debugging.
+
+    Parameters
+    ----------
+    labels : np.ndarray
+        The labels for the records, of shape `(num_samples,)` with binary values.
+    probability_outputs : np.ndarray
+        The probability outputs for the records, of shape `(num_samples,)` with probabilities (0 to 1).
+    max_fraction_positive : float, default 0.05
+        The maximum fraction of positive instances allowed.
+
+    Returns
+    -------
+    float
+        The computed challenge score for the "chagas" prediction task.
+
+    """
+    # Check the data.
+    assert len(labels) == len(probability_outputs)
+    num_instances = len(labels)
+    max_num_positive_instances = int(max_fraction_positive * num_instances)
+
+    # Convert the data to NumPy arrays, as needed, for easier indexing.
+    labels = np.asarray(labels, dtype=np.float64)
+    probability_outputs = np.asarray(probability_outputs, dtype=np.float64)
+
+    # Collect the unique output values as the thresholds for the positive and negative classes.
+    thresholds = np.unique(probability_outputs)
+    thresholds = np.append(thresholds, thresholds[-1] + 1)
+    thresholds = thresholds[::-1]
+    num_thresholds = len(thresholds)
+
+    idx = np.argsort(probability_outputs)[::-1]
+
+    # Initialize the TPs, FPs, FNs, and TNs with no positive outputs.
+    tp = np.zeros(num_thresholds)
+    fp = np.zeros(num_thresholds)
+    fn = np.zeros(num_thresholds)
+    tn = np.zeros(num_thresholds)
+
+    tp[0] = 0
+    fp[0] = 0
+    fn[0] = np.sum(labels == 1)
+    tn[0] = np.sum(labels == 0)
+
+    # Update the TPs, FPs, FNs, and TNs using the values at the previous threshold.
+    i = 0
+    for j in range(1, num_thresholds):
+        tp[j] = tp[j - 1]
+        fp[j] = fp[j - 1]
+        fn[j] = fn[j - 1]
+        tn[j] = tn[j - 1]
+
+        while i < num_instances and probability_outputs[idx[i]] >= thresholds[j]:
+            if labels[idx[i]] == 1:
+                tp[j] += 1
+                fn[j] -= 1
+            else:
+                fp[j] += 1
+                tn[j] -= 1
+            i += 1
+
+    # Find the true positive rate so that the number of positive model outputs are no more than 5% of the total instances.
+    k = num_thresholds
+    for j in range(1, num_thresholds):
+        if tp[j] + fp[j] > max_num_positive_instances:
+            k = j - 1
+            break
+
+    print(f"Cutoff probability: {thresholds[k]}")
+
+    if tp[k] + fn[k] > 0:
+        tpr = tp[k] / (tp[k] + fn[k])
+    else:
+        tpr = float("nan")
+
+    return tpr
 
 
 def compute_arr_diag_metrics(
